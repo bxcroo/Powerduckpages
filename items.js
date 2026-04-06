17b40784be0503ecbd3aaf65aed39fc4993db4b4
beaux <bxcroo@gmail.com>
2026-04-06 11:49:10 +1000

    Update content feed 2026-04-06T01:49:10.829Z

--- items.js
+++ items.js
@@ -193,10 +193,10 @@ window.POWERDUCK_ITEMS = [
     detailLabel: "More from me"
   },
   {
-    title: "Gear - FiiO JadeAudio JT7 Open-Back Planar Headphones",
-    category: "",
+    title: "FiiO JadeAudio JT7 Open-Back Planar Headphones",
+    category: "Gear",
     description: "These are planar headphones. They're ridiculously comfortable and sound incredible. Very hard to beat in terms of bang for your buck. The build quality is also fantastic. They don't creak or flex around. Just damn good headphones especially at this pricepoint!",
-    image: "images/generated/gear-fiio-jadeaudio-jt7-open-back-planar-headphones-9c09f191.png",
+    image: "images/generated/gear-fiio-jadeaudio-jt7-open-back-planar-headphones-13b1ed7b.png",
     url: "https://apos.audio/products/fiio-jadeaudio-jt7-open-back-planar-headphones",
     buttonLabel: "Go and See",
     slug: "gear-fiio-jadeaudio-jt7-open-back-planar-headphones",
--- posts/gear-fiio-jadeaudio-jt7-open-back-planar-headphones.js
+++ posts/gear-fiio-jadeaudio-jt7-open-back-planar-headphones.js
@@ -1,15 +1,15 @@
 window.POWERDUCK_POST = {
   slug: "gear-fiio-jadeaudio-jt7-open-back-planar-headphones",
-  title: "Gear - FiiO JadeAudio JT7 Open-Back Planar Headphones",
+  title: "FiiO JadeAudio JT7 Open-Back Planar Headphones",
   subtitle: "",
   description: "These are planar headphones. They're ridiculously comfortable and sound incredible. Very hard to beat in terms of bang for your buck. The build quality is also fantastic. They don't creak or flex around. Just damn good headphones especially at this pricepoint!",
   body: "",
-  image: "images/generated/gear-fiio-jadeaudio-jt7-open-back-planar-headphones-9c09f191.png",
-  imageAlt: "Gear - FiiO JadeAudio JT7 Open-Back Planar Headphones",
+  image: "images/generated/gear-fiio-jadeaudio-jt7-open-back-planar-headphones-13b1ed7b.png",
+  imageAlt: "FiiO JadeAudio JT7 Open-Back Planar Headphones",
   imageAspect: "landscape",
   imageFit: "contain",
   buttonLabel: "Go and See",
   detailLabel: "More from me",
-  category: "",
+  category: "Gear",
   url: "https://apos.audio/products/fiio-jadeaudio-jt7-open-back-planar-headphones"
 };
