0e63952a1dc2457ac7cc1b1aacfca04758e57e59
beaux <bxcroo@gmail.com>
2026-04-06 11:03:54 +1000

    Update content feed 2026-04-06T01:03:54.400Z

--- items.js
+++ items.js
@@ -51,10 +51,10 @@ window.POWERDUCK_ITEMS = [
     detailLabel: "More from me"
   },
   {
-    title: "Gear - Kobo Clara BW",
-    category: "",
+    title: "Kobo Clara BW",
+    category: "Gear",
     description: "Amazon Kindle? Nah. Kobo Clara! Install KoReader on it for all the untethered goodness you could want. I’ve installed a file manage plugin so I can send ePub format books straight to it from my phone over Wifi. Custom screensavers and one touch immediate return to your last reading position. Oh and if you have multiple eReaders you can sync them with KoReader so they all resume from the same point! Keen for my Xteink X4 to turn up for more convenient reading on the go!",
-    image: "images/generated/gear-kobo-clara-bw-62641fe8.png",
+    image: "images/generated/gear-kobo-clara-bw-c44efc37.png",
     url: "https://us.kobobooks.com/products/kobo-clara-bw?utm_source=Kobo&utm_medium=TopNav&utm_campaign=ClaraBW",
     buttonLabel: "Go and See",
     detailUrl: "post.html?slug=gear-kobo-clara-bw",
--- posts/gear-kobo-clara-bw.js
+++ posts/gear-kobo-clara-bw.js
@@ -1,15 +1,15 @@
 window.POWERDUCK_POST = {
   slug: "gear-kobo-clara-bw",
-  title: "Gear - Kobo Clara BW",
+  title: "Kobo Clara BW",
   subtitle: "",
   description: "Amazon Kindle? Nah. Kobo Clara! Install KoReader on it for all the untethered goodness you could want. I’ve installed a file manage plugin so I can send ePub format books straight to it from my phone over Wifi. Custom screensavers and one touch immediate return to your last reading position. Oh and if you have multiple eReaders you can sync them with KoReader so they all resume from the same point! Keen for my Xteink X4 to turn up for more convenient reading on the go!",
   body: "",
-  image: "images/generated/gear-kobo-clara-bw-62641fe8.png",
-  imageAlt: "Gear - Kobo Clara BW",
+  image: "images/generated/gear-kobo-clara-bw-c44efc37.png",
+  imageAlt: "Kobo Clara BW",
   imageAspect: "landscape",
   imageFit: "contain",
   buttonLabel: "Go and See",
   detailLabel: "More from me",
-  category: "",
+  category: "Gear",
   url: "https://us.kobobooks.com/products/kobo-clara-bw?utm_source=Kobo&utm_medium=TopNav&utm_campaign=ClaraBW"
 };
