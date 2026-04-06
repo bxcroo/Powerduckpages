b382ed836378e63deb680a4251f8043ac0528a3e
beaux <bxcroo@gmail.com>
2026-04-06 11:45:26 +1000

    Update content feed 2026-04-06T01:45:26.000Z

--- items.js
+++ items.js
@@ -219,12 +219,17 @@ window.POWERDUCK_ITEMS = [
     detailLabel: "More from me"
   },
   {
-    title: "Gear - GK Kunten In Ear Monitors",
-    category: "",
+    title: "GK Kunten In Ear Monitors",
+    category: "Gear",
     description: "These things punch waaayyyy above their weight. Squeezing out every bit of bang for your buck the GK Kunten is fantastic complimentary IEM for the Snowsky Echo Mini. I usually have AirPod pro 2s in but it was astonishing to see how much sound quality I had traded out for convenience. They sound SO much better than my AirPods.",
-    image: "images/GKKUNTEN.png",
+    image: "images/generated/gear-gk-kunten-in-ear-monitors-d5bb9377.png",
     url: "https://www.head-fi.org/showcase/gk-kunten.28787/",
-    buttonLabel: "Go and see"
+    buttonLabel: "Go and see",
+    slug: "gear-gk-kunten-in-ear-monitors",
+    imageAspect: "landscape",
+    imageFit: "contain",
+    detailUrl: "post.html?slug=gear-gk-kunten-in-ear-monitors",
+    detailLabel: "More from me"
   },
   {
     title: "Gear - Xteink X4",
---
+++ posts/gear-gk-kunten-in-ear-monitors.js
@@ -0,0 +1,15 @@
+window.POWERDUCK_POST = {
+  slug: "gear-gk-kunten-in-ear-monitors",
+  title: "GK Kunten In Ear Monitors",
+  subtitle: "",
+  description: "These things punch waaayyyy above their weight. Squeezing out every bit of bang for your buck the GK Kunten is fantastic complimentary IEM for the Snowsky Echo Mini. I usually have AirPod pro 2s in but it was astonishing to see how much sound quality I had traded out for convenience. They sound SO much better than my AirPods.",
+  body: "",
+  image: "images/generated/gear-gk-kunten-in-ear-monitors-d5bb9377.png",
+  imageAlt: "GK Kunten In Ear Monitors",
+  imageAspect: "landscape",
+  imageFit: "contain",
+  buttonLabel: "Go and see",
+  detailLabel: "More from me",
+  category: "Gear",
+  url: "https://www.head-fi.org/showcase/gk-kunten.28787/"
+};
