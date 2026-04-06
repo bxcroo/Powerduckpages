0d4527ff1f263c19c51539435791cbe70078622c
beaux <bxcroo@gmail.com>
2026-04-06 11:21:13 +1000

    Update content feed 2026-04-06T01:21:13.327Z

--- posts/gear-kobo-clara-bw.js
+++ posts/gear-kobo-clara-bw.js
@@ -4,7 +4,7 @@ window.POWERDUCK_POST = {
   subtitle: "",
   description: "Amazon Kindle? Nah. Kobo Clara! Install KoReader on it for all the untethered goodness you could want. I’ve installed a file manage plugin so I can send ePub format books straight to it from my phone over Wifi. Custom screensavers and one touch immediate return to your last reading position. Oh and if you have multiple eReaders you can sync them with KoReader so they all resume from the same point! Keen for my Xteink X4 to turn up for more convenient reading on the go!",
   body: "",
-  image: "images/generated/gear-kobo-clara-bw-c44efc37.png",
+  image: "images/generated/gear-kobo-clara-bw-265f2eb4.png",
   imageAlt: "Kobo Clara BW",
   imageAspect: "landscape",
   imageFit: "contain",
