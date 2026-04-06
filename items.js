817df11d1399acc5d5c5e51bf221bf033ef330a3
beaux <bxcroo@gmail.com>
2026-04-06 11:36:17 +1000

    Update content feed 2026-04-06T01:36:17.693Z

--- items.js
+++ items.js
@@ -90,10 +90,10 @@ window.POWERDUCK_ITEMS = [
     detailLabel: "More from me"
   },
   {
-    title: "Gear - Lodge Cast Iron",
-    category: "",
+    title: "Lodge Cast Iron",
+    category: "Gear",
     description: "Such a classic. Buy it for life. Doesn't matter what you choose from Lodges line, it's going to outlast you. Cast iron does need a little extra love compared to the nonstick stuff but just keep using it and wiping it out with a paper towel. A good cast iron pan retains heat, works on any hob and is admirably non stick when you get it up to temp. Learn how to use it and you'll never look back.",
-    image: "images/generated/gear-lodge-cast-iron-446a2c2d.png",
+    image: "images/generated/gear-lodge-cast-iron-cb501213.png",
     url: "https://www.lodgecastiron.com/",
     slug: "gear-lodge-cast-iron",
     imageAspect: "landscape",
